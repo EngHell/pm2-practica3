8ccb460d78e05573b656bf412562306d0188c3f3
Angel Recinos <migueln6@gmail.com>
2021-05-05 20:27:17 -0600

    xreated new model for uploads

--- automata/models.py
+++ automata/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+MyUser = get_user_model()
+
+
+def get_file_upload_file_name(instance, filename) -> str:
+    return f'uploads/automata/{instance.user.id}/{now().timestamp()}.p2'
+
+
+class Uploads(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
+    file = models.FileField(upload_to=get_file_upload_file_name)
+
+    def __str__(self):
+        return f"{self.name} - {self.user}"
